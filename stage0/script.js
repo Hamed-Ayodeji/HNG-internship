fb3fa5f96d1d335c55d78a78543175f60c9d6bd8
Ayodeji Hamed <ayodejihamed@gmail.com>
2024-06-29 17:41:38 +0100

    minor fixes

--- stage0/script.js
+++ stage0/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const name = "Hamed Ayodeji A.";
-  const username = "Qurtana | DevOps";
+  const username = "@Ayodeji Hamed";
   const email = "ayodejihamed@gmail.com";
 
   document.getElementById("name").textContent = name;
