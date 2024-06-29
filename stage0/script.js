e893856cfab5a86b24ec0a4c9054771fa2e41bd2
Ayodeji Hamed <ayodejihamed@gmail.com>
2024-06-29 17:00:58 +0100

    first task

---
+++ stage0/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const name = "Hamed Ayodeji A.";
+  const username = "Qurtana | DevOps";
+  const email = "ayodejihamed@gmail.com";
+
+  document.getElementById("name").textContent = name;
+  document.getElementById("username").textContent = username;
+  document.getElementById("email").textContent = email;
+});
